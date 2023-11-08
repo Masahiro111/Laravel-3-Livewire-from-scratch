2038365735d8ecf1f265fa51b079178cb179b933
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2023-11-08 18:28:20 +0900

    Lazy loading

--- app/Livewire/NavBar.php
+++ app/Livewire/NavBar.php
@@ -2,12 +2,25 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Lazy;
 use Livewire\Component;
 
 class NavBar extends Component
 {
+    #[Lazy]
+    public function  placeholder()
+    {
+        return <<<'HTML'
+        <div class="placeholder-glow">
+            <div class="placeholder col-12">aaa
+            </div>
+        </div>
+        HTML;
+    }
+
     public function render()
     {
+        sleep(5);
         return view('livewire.nav-bar');
     }
 }
